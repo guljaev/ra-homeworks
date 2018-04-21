2548bedaaabe76cfc82584b7ae304a5cdfdc1a95
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-04-21 12:41:01 +0300

    1.3 func-component/chat is done

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,30 @@
 'use strict';
+
+function MessageHistory({ list }) {
+    // console.log(list);
+    if (!list.length) return null;
+
+    const messageList = list.map(msg => {
+        const message = {
+            time: msg.time,
+            text: msg.text
+        };
+
+        switch (msg.type) {
+            case 'message':
+                return <Message key={msg.id} from={msg.from} message={message} />;
+                break;
+            case 'response':
+                return <Response key={msg.id} from={msg.from} message={message} />;
+                break;
+            case 'typing':
+                return <Typing key={msg.id} from={msg.from} message={message} />;
+                break;
+            default:
+                return null;
+                break;
+        }
+    });
+
+    return <ul>{messageList}</ul>;
+}
