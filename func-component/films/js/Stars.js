29e26a46d1a4ab5adffa7d836e0f792f0fcfeb63
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-04-21 17:48:19 +0300

    1.3 func-component/films: apply shortid

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -8,17 +8,10 @@ function Stars({ count }) {
   }
 
   let starList = new Array(Math.round(count)).fill(undefined);
-  starList = starList.map(() => <li><Star /></li>);
+  starList = starList.map(() => <li key={shortid.generate()}><Star /></li>);
   // можно ли так делать - в переменную starList присваивать ссылку на массив, полученный из этого же starList?
 
-  // const starListIndexed = keyIndex(starList, 1);
-  // console.log(starListIndexed);
-  // const starListIndexed = starList.map((star) => {
-  //   return { id: shortid.generate(), value: star };
-  // });
-  // console.log(starListIndexed);
-  // не смог использовать keyIndex или shortid.generate()   =(
-
+  
   return (
     <ul className="card-body-stars u-clearfix">
       {starList}
