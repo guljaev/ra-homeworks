a37bfc29d93ef72d597154b18e8b76f415208a52
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-04-21 11:40:10 +0300

    1.3 func-component/films is done

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,5 +1,27 @@
 'use strict';
 
-function Stars() {
-  return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
+// import keyIndex from 'react-key-index';
+
+function Stars({ count }) {
+  if ((typeof count !== 'number') || (count < 1) || (count > 5)) {
+    return null;
+  }
+
+  let starList = new Array(Math.round(count)).fill(undefined);
+  starList = starList.map(() => <li><Star /></li>);
+  // можно ли так делать - в переменную starList присваивать ссылку на массив, полученный из этого же starList?
+
+  // const starListIndexed = keyIndex(starList, 1);
+  // console.log(starListIndexed);
+  // const starListIndexed = starList.map((star) => {
+  //   return { id: shortid.generate(), value: star };
+  // });
+  // console.log(starListIndexed);
+  // не смог использовать keyIndex или shortid.generate()   =(
+
+  return (
+    <ul className="card-body-stars u-clearfix">
+      {starList}
+    </ul>
+  );
 }
