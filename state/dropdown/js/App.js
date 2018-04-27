698052613fedf076a885e89c2f9ed64282d2f8cc
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-04-27 14:40:36 +0300

    2.1 state/dropdown is done

--- state/dropdown/js/App.js
+++ state/dropdown/js/App.js
@@ -1,28 +1,31 @@
-var App = React.createClass({
-  defaultProps: {
-    options: []
-  },
-  getInitialState: function () {
-    return {
+'use strict';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       active: this.props.options[0],
       open: false
     };
-  },
-  handleChange: function (option) {
+  }
+
+  handleChange(option) {
     this.setState({
       active: option
     });
-  },
-  toggleOpen: function () {
+  }
+
+  toggleOpen() {
     this.setState({
       open: !this.state.open
     });
-  },
-  render: function () {
+  }
+
+  render() {
     return (
       <div className="container">
         <div className={`dropdown-wrapper ${this.state.open ? "open" : ""}`} >
-          <button className={"btn"} onClick={this.toggleOpen} >
+          <button className={"btn"} onClick={this.toggleOpen.bind(this)} >
             <span>Account Settings</span>
             <i className="material-icons">public</i>
           </button>
@@ -39,4 +42,8 @@ var App = React.createClass({
       </div>
     );
   }
-});
+}
+
+App.defaultProps = {
+  options: []
+};
