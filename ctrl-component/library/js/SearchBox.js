42a6fdfc79c1c927e0faaa2d408997f4736e3be0
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-05-06 13:11:31 +0300

    2.3 ctrl-component/library is done

--- ctrl-component/library/js/SearchBox.js
+++ ctrl-component/library/js/SearchBox.js
@@ -1,5 +1,10 @@
-const SearchBox = () => {
-    return (
-        <input type="text" placeholder="Поиск по названию или автору"/>
-    );
+const SearchBox = (props) => {
+	return (
+			<input 
+					type="text" 
+					placeholder="Поиск по названию или автору"
+					value={props.value}
+					onChange={event => props.filterBooks(event.currentTarget.value)}
+			/>
+	);
 };
