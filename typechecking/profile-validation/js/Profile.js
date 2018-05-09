eb6a81c5f2c175c5726423f8cdd6ba20f1152db5
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-05-09 15:17:31 +0300

    2.4 typechecking/profile-validation is done

--- typechecking/profile-validation/js/Profile.js
+++ typechecking/profile-validation/js/Profile.js
@@ -27,3 +27,59 @@ const Profile = props => {
     </div>
   );
 };
+
+
+const urlPropType = (props, propName, componentName) => {
+  const url = props[propName];
+  const isUrlCorrect = (typeof url === 'string') && /^https:\/\/vk.com\/(id[0-9]+|[A-Za-z0-9_-]+)$/.test(url);
+  if (!isUrlCorrect) {
+    return new Error(`Неверный формат параметра ${propName} в компоненте ${componentName}: необходим указать корректный url`);
+  }
+  return null;
+};
+
+const createChainableTypeChecker = (validate) => {
+  const checkType = (isRequired, props, propName, componentName) => {
+    if (!props[propName]) {
+      if (isRequired) {
+        return new Error(`Обязательный атрибут ${propName} не был передан компоненту ${componentName}`)
+      }
+      return null;
+    } else {
+      return validate(props, propName, componentName);
+    }
+  };
+  const chainedCheckType = checkType.bind(null, false);
+  chainedCheckType.isRequired = checkType.bind(null, true);
+  return chainedCheckType;
+};
+
+const birthdayPropType = (props, propName, componentName) => {
+  const birthday = props[propName];
+  const isBirthday = (typeof birthday === 'string') && /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/.test(birthday);
+  const isBirthdayLowerThanToday = () => {
+    const bDate = new Date(birthday.slice(0, 4), +birthday.slice(5, 7) - 1, birthday.slice(8, 10));
+    return bDate < new Date();
+  }
+
+  if (!isBirthday) {
+    return new Error(`Неверный формат параметра ${propName} в компоненте ${componentName}: необходим указать дату рождения в формате YYYY-MM-DD`);
+  } else if (!isBirthdayLowerThanToday()) {
+    return new Error(`Неверн указан параметр ${propName} в компоненте ${componentName}: дата рождения не должна быть больше текущей`);
+  }
+  return null;
+}
+
+const birthdayPropTypeChecker = createChainableTypeChecker(birthdayPropType);
+
+Profile.propTypes = {
+  first_name: PropTypes.node,
+  last_name: PropTypes.node,
+  img: PropTypes.string,
+  url: urlPropType,
+  birthday: birthdayPropTypeChecker
+};
+
+Profile.defaultProps = {
+  img: './images/profile.jpg'
+}
