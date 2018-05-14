b1cab57835a2ff5473477a8fa53ff669e88a8ef9
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-05-14 22:40:03 +0300

    RA 1.3 events/auth is redone

--- events/auth/js/AuthForm.js
+++ events/auth/js/AuthForm.js
@@ -1,18 +1,17 @@
 'use strict';
 
 function AuthForm({ onAuth }) {
-    let textField,
-    emailField,
-    passwordField;
-
     function authUser(event) {
         event.preventDefault();
         if ( (!onAuth) || (typeof onAuth !== 'function') ) return null;
+
+        const formElements = event.currentTarget.elements;
         const user = {
-            name: textField.value,
-            email: emailField.value,
-            password: passwordField.value
+            name: formElements.name.value,
+            email: formElements.email.value,
+            password: formElements.password.value
         };
+
         onAuth(user);
     }
 
@@ -29,15 +28,15 @@ function AuthForm({ onAuth }) {
     return (
         <form className="ModalForm" onSubmit={authUser} action="/404/auth/" method="POST">
             <div className="Input">
-                <input ref={elem => textField = elem} required type="text" placeholder="Имя" />
+                <input required name="name" type="text" placeholder="Имя" />
                 <label></label>
             </div>
             <div className="Input">
-                <input ref={elem => emailField = elem} onChange={checkEmailField} type="email" placeholder="Электронная почта" />
+                <input onChange={checkEmailField} name="email" type="email" placeholder="Электронная почта" />
                 <label></label>
             </div>
             <div className="Input">
-                <input ref={elem => passwordField = elem} onChange={checkPasswordField} required type="password" placeholder="Пароль" />
+                <input onChange={checkPasswordField} required name="password" type="password" placeholder="Пароль" />
                 <label></label>
             </div>
             <button type="submit">
