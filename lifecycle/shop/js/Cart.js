e2e0e29f6f38d958df5669fc51ac6b972968a0e7
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-05-19 19:08:39 +0300

    RA 3.2 lifecycle/shop is done

--- lifecycle/shop/js/Cart.js
+++ lifecycle/shop/js/Cart.js
@@ -1,4 +1,15 @@
 class Cart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return (this.props.isOpen || nextProps.isOpen)
+      ? (this.props.isOpen && nextProps.isOpen)
+        ? (this.props.items.length !== nextProps.items.length)
+        : true
+      : false;
+  }
 
   render() {
     return (
