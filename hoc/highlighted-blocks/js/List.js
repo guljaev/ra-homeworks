abfc8eac24450e01fae664b478cff37d058557c5
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-05-20 23:19:42 +0300

    RA 3.3 hoc/highlighted-blocks is done

--- hoc/highlighted-blocks/js/List.js
+++ hoc/highlighted-blocks/js/List.js
@@ -1,16 +1,45 @@
 'use strict';
 
+const Usual = props => props.children;
+
+const modifyView = (Component) => class modifiedViewComponent extends React.Component {
+    constructor(props) {
+		super(props);
+		this.state = {};
+    }
+
+    componentWillMount() {
+        const views = this.props.views;
+        let ComponentOuter = Usual;
+        if (views < 100) ComponentOuter = New;
+        if (views >= 1000) ComponentOuter = Popular;
+        this.setState({ componentOuter: ComponentOuter });
+    }
+    
+    render() {
+        const ComponentOuter = this.state.componentOuter;
+        return (
+            <ComponentOuter>
+                <Component {...this.props} />
+            </ComponentOuter>
+        );
+	}
+};
+
+const ModifiedViewArticle = modifyView(Article);
+const ModifiedViewVideo = modifyView(Article);
+
 const List = props => {
     return props.list.map(item => {
         switch (item.type) {
             case 'video':
                 return (
-                    <Video {...item} />
+                    <ModifiedViewVideo {...item} />
                 );
 
             case 'article':
                 return (
-                    <Article {...item} />
+                    <ModifiedViewArticle {...item} />
                 );
         }
     });
