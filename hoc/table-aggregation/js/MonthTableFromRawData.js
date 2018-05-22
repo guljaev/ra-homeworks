040e0ab621c90877e4fd638a2df913156fa85bca
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-05-22 22:20:05 +0300

    RA 3.3 hoc/table-aggregation: Correct output data for first and second columns

--- hoc/table-aggregation/js/MonthTableFromRawData.js
+++ hoc/table-aggregation/js/MonthTableFromRawData.js
@@ -1,17 +1,34 @@
 'use strict';
 
 function getMonthData(props) {
-  const monthList = props.list.map(item => {
-      const date = new Date(item.date);
-      const options = {
-          month: 'short'
-      };
-      const month = date.toLocaleString('en', options);
-      return {
-          month: month,
-          amount: item.amount
-      };
-  });
+  // const monthList = props.list.map(item => {
+  //     const date = new Date(item.date);
+  //     const options = {
+  //         month: 'short'
+  //     };
+  //     const month = date.toLocaleString('en', options);
+  //     return {
+  //         month: month,
+  //         amount: item.amount
+  //     };
+	// });
+	const monthList = [];
+	props.list.forEach( item => {
+		const date = new Date(item.date);
+		const options = {
+			month: 'short'
+		};
+		const monthStr = date.toLocaleString('en', options);
+		const monthIndex = date.getMonth();
+		if (monthList[monthIndex]) {
+			monthList[monthIndex].amount += item.amount;
+		} else {
+			monthList[monthIndex] = {
+				month: monthStr,
+				amount: item.amount
+			};
+		}
+	});
   return { list: monthList };
 }
 const MonthTableFromRawData = fromRawData(getMonthData)(MonthTable);
--- hoc/table-aggregation/js/YearTableFromRawData.js
+++ hoc/table-aggregation/js/YearTableFromRawData.js
@@ -1,17 +1,31 @@
 'use strict';
 
 function getYearData(props) {
-  const yearList = props.list.map(item => {
-      const date = new Date(item.date);
-      const options = {
-        year: 'numeric'
-      };
-      const year = date.toLocaleString('en', options);
-      return {
-          year: year,
-          amount: item.amount
-      };
-  });
+  // const yearList = props.list.map(item => {
+  //     const date = new Date(item.date);
+  //     const options = {
+  //       year: 'numeric'
+  //     };
+  //     const year = date.toLocaleString('en', options);
+  //     return {
+  //         year: year,
+  //         amount: item.amount
+  //     };
+  // });
+  const yearListObj = props.list.reduce( (newList, item) => {
+		const date = new Date(item.date);
+		const options = {
+			year: 'numeric'
+		};
+		const year = date.toLocaleString('en', options);
+		newList[year] = (newList[year]) ? newList[year] + item.amount : item.amount;
+		return newList;
+  }, {});
+  const yearList = Object.keys(yearListObj)
+    .sort( (year1, year2) => +year1 > +year2)
+    .map(year => { 
+      return { year: year, amount: yearListObj[year] };
+    });
   return { list: yearList };
 }
 const YearTableFromRawData = fromRawData(getYearData)(YearTable);
