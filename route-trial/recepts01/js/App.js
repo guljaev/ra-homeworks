44fce94eb752dd42a9ef1a9f95569b0a19fb57f1
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-05-30 20:54:51 +0300

    try to connect

--- route-trial/recepts01/js/App.js
+++ route-trial/recepts01/js/App.js
@@ -1,11 +1,42 @@
 'use strict';
 
+const ReactRouter = window.ReactRouterDOM;
+// const {Route, Switch, BrowserRouter} = ReactRouterDOM;
+const Router = ReactRouterDOM.HashRouter;
+const Route = ReactRouterDOM.Route;
+
+const RecipesList = () => (
+    <div>
+        <h1>Рецепты</h1>
+        <ul>
+            <li>Борщ</li>
+            <li>Утка по-египетски</li>
+            <li>рыба-шалунья</li>
+        </ul>
+    </div>
+);
+
+const NewRecipe = () => (
+    <div>
+        <div>Новый рецепт</div>
+        <textarea name="recipe" id="recipe" cols="30"_ />
+    </div>
+);
+
 class App extends React.Component {
-    render () {
+    render() {
         return (
-            <div className="App">
-                <SubscribeForm/>
-            </div>
+            <Router>
+                <div>
+                    <Route path="recipes" component={RecipesList} />
+                    <Route path="new" component={NewRecipe} />
+                </div>
+            </Router>
         );
     }
 }
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
