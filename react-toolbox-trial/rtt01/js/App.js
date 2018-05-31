3ab9272b2e282aba1bfd6e4cb2a903cf923cec14
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-05-31 21:06:08 +0300

    trying to connect to toolbox package

--- react-toolbox-trial/rtt01/js/App.js
+++ react-toolbox-trial/rtt01/js/App.js
@@ -1,10 +1,86 @@
 'use strict';
 
+import Autocomplete from 'react-toolbox/lib/autocomplete';
+console.log(Autocomplete);
+
+// const source = {
+//   'ES-es': 'Spain',
+//   'TH-th': 'Thailand',
+//   'EN-gb': 'England',
+//   'EN-en': 'USA'
+// };
+
+// class AutocompleteTest extends React.Component {
+//   state = {
+//     countries: ['ES-es', 'TH-th']
+//   }
+
+//   handleChange = (value) => {
+//     this.setState({countries: value});
+//   };
+
+//   render () {
+//     return (
+//       <Autocomplete
+//         direction="down"
+//         selectedPosition="above"
+//         label="Choose countries"
+//         onChange={this.handleChange}
+//         source={source}
+//         value={this.state.countries}
+//       />
+//     );
+//   }
+// }
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// const Autocomplete = () => (
+//     <div>
+//         Тип кваритры: <br />
+//         <select>
+//             <option>Квартира в новостройке</option>
+//             <option>Готовая квартира</option>
+//             <option>Загородный дом</option>
+//         </select>
+//     </div>
+// );
+
+const Inputs = () => (
+    <div>
+        <label>
+            Стоимость:
+            <input type="number" name="price" value="2000000" />
+            руб.
+        </label>
+        <label>
+            На руках:
+            <input type="number" name="money" value="200000" />
+            руб.
+        </label>
+        <label>
+            Срок кредита:
+            <input type="number" name="duration" value="5" />
+            лет
+        </label>
+    </div>
+);
+
+const Button = () => (
+    <button>
+        Отправить
+    </button>
+);
+
 class App extends React.Component {
-    render () {
+    render() {
         return (
             <div className="App">
-                <SubscribeForm/>
+                <form action="">
+                    {/* <Autocomplete /> */}
+                    <Inputs />
+                    <Button />
+                </form>
             </div>
         );
     }
