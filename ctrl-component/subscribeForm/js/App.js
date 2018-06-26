4ae8be3ec0f16bdfafcb1049cb94d61c4e260b7e
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-06-26 20:32:19 +0300

    ничего важного

--- ctrl-component/subscribeForm/js/App.js
+++ ctrl-component/subscribeForm/js/App.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
 class App extends React.Component {
     render () {
         return (
--- ctrl-component/subscribeForm/js/SubscribeForm.js
+++ ctrl-component/subscribeForm/js/SubscribeForm.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
 class SubscribeForm extends React.Component {
 	constructor(props) {
 		super(props);
